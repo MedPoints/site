f20a3ec9cf2eb858a70014150a37e06c814c45ad
Nikita Namestnikov <nikita.namestnikov@onetwotrip.com>
2018-07-01 14:29:09 +0300

    doctors view

--- controllers/doctors.js
+++ controllers/doctors.js
@@ -1,5 +1,6 @@
+const config = require('config');
 const axios = require('axios');
-const API_URL = process.env.API_URL;
+const API_URL = config.get('API_URL');
 
 exports.getDoctors = async (req, res) => {
   const request = await axios.get(`${API_URL}/api/doctors`);
@@ -11,5 +12,20 @@ exports.getDoctor = async (req, res) => {
   const id = req.params.id;
   const request = await axios.get(`${API_URL}/api/doctors?id=${id}`);
   const doctor = request.data.result;
+  const rate = doctor.ratings.reduce((result, rate) => {
+    const {knowledge, skills, attention, priceQuality} = rate.commonRate;
+    result.knowledge += knowledge;
+    result.skills += skills;
+    result.attention += attention;
+    result.priceQuality += priceQuality;
+    return result;
+  }, {knowledge: 0, skills: 0, attention: 0, priceQuality: 0});
+  if (doctor.ratings.length !== 0) {
+	  rate.knowledge /= doctor.ratings.length;
+	  rate.skills /= doctor.ratings.length;
+	  rate.attention /= doctor.ratings.length;
+	  rate.priceQuality /= doctor.ratings.length;
+  }
+  doctor.opinion = rate;
   res.render('doctors/doctor', { doctor });
-}
+};
--- handlers/handlebars-helpers.js
+++ handlers/handlebars-helpers.js
@@ -1,3 +1,44 @@
+const moment = require('moment');
+
 exports.hbsHelpers = {
-  activeLink: (url, path) => url.startsWith(path) ? 'active' : ''
+  activeLink: (url, path) => url.startsWith(path) ? 'active' : '',
+  times: (n, block) => {
+    let accum = '';
+    for (let i = 0; i < n; i++) {
+      accum += block.fn(i);
+    }
+    return accum;
+  },
+  dateFormat: (date) => {
+    return moment(new Date(date)).format('D MMMM YYYY');
+  },
+  composeRate: (doctor) => {
+    if (doctor.ratings.length === 0) {
+      return {knowledge: 0, skills: 0, attention: 0, priceQuality: 0};
+    }
+    const rate = doctor.ratings.reduce((result, rate) => {
+      const {knowledge, skills, attention, priceQuality} = rate.commonRate;
+      result.knowledge += knowledge;
+      result.skills += skills;
+      result.attention += attention;
+      result.priceQuality += priceQuality;
+    }, {knowledge: 0, skills: 0, attention: 0, priceQuality: 0});
+    rate.knowledge /= doctor.ratings.length;
+    rate.skills /= doctor.ratings.length;
+    rate.attention /= doctor.ratings.length;
+    rate.priceQuality /= doctor.ratings.length;
+    return rate;
+  },
+  getProgressBarColor: (skill) => {
+    if(skill >= 7) {
+      return 'bg-success';
+    } else if (skill >= 5) {
+      return 'bg-warning';
+    } else {
+      return 'bg-danger';
+    }
+  },
+  getProgressBarWidth: (skill) => {
+    return skill * 10;
+  }
 };
