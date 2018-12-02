0be6d196198ea1c096270271f4227b101d855823
Maxim Kotov <maxim.kotov@tutanota.com>
2018-12-03 00:14:19 +0300

    Added favorites support

--- routes/account.js
+++ routes/account.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const account = require('../controllers/account');
+const favorites = require('../controllers/favourites');
 const catchErrors = require('../handlers/errorHandlers').catchErrors;
 
 router.get('/', catchErrors(account.getAccountInfo));
@@ -7,6 +8,7 @@ router.get('/edit', catchErrors(account.editInfo));
 router.get('/records', catchErrors(account.records));
 router.post('/updateAccount', catchErrors(account.updateAccount));
 router.get('/success', catchErrors(account.success));
+router.get('/favorites', catchErrors(favorites.getFavorites));
 router.get('/getUserAppointments', catchErrors(account.getAccountAppointments));
 
 module.exports = router;
