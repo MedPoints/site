d461c3613d6cd1cd9a316bf5aff92a0cb3691447
k0t0v <maxim.kotov@tutanota.com>
2018-12-18 02:49:24 +0300

    fix foundations

--- routes/foundation.js
+++ routes/foundation.js
@@ -4,7 +4,18 @@ const catchErrors = require('../handlers/errorHandlers').catchErrors;
 
 router.get('/', catchErrors(foundation.getFoundationInfo));
 router.get('/cases', catchErrors(foundation.getFoundationCases));
-router.get('/cases/:id', catchErrors(foundation.getFoundationCase));
 
+//test-pages
+router.get('/cases/julia-ostin', async (req, res) => {
+    res.render('foundation/test-pages/julia-ostin', {title: `MedPoints™ Foundation Case`,req,});
+});
+router.get('/cases/sam-pitt', async (req, res) => {
+    res.render('foundation/test-pages/sam-pitt', {title: `MedPoints™ Foundation Case`,req,});
+});
+router.get('/cases/jessy-smith', async (req, res) => {
+    res.render('foundation/test-pages/jessy-smith', {title: `MedPoints™ Foundation Case`,req,});
+});
+
+router.get('/cases/:id', catchErrors(foundation.getFoundationCase));
 
 module.exports = router;
