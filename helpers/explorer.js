c59486e0a1f2fab611349b53e5927947af218bd5
k0t0v <maxim.kotov@tutanota.com>
2018-12-23 15:30:57 +0300

    add explorer table column with dates

--- helpers/explorer.js
+++ helpers/explorer.js
@@ -3,6 +3,7 @@ const moment = require('moment');
 exports.prepareBlockData = (block, index, chainLength) => {
     return {
         ...block,
+        date: moment(new Date(block.Time)).format('HH:mm DD.MM.YYYY'),
         created: moment(new Date(block.Time)).fromNow(),
         height: chainLength - index,
         transactionsCount: block.Transactions.length,
