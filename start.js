6cd3c323b50971b873a410a1d55d958c71892be8
k0t0v <maxim.kotov@tutanota.com>
2020-03-08 19:15:15 +0300

    https server

--- start.js
+++ start.js
@@ -1,10 +1,41 @@
 // import environmental variables from our variables.env file
 require('dotenv').config({ path: 'variables.env' });
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 
-// Start our app!
+
 const app = require('./app');
 app.set('port', process.env.PORT || 80);
 app.set('host', process.env.HOST || '0.0.0.0');
-const server = app.listen(app.get('port'), app.get('host'), () => {
-    console.log(`Express running → PORT ${server.address().port}`);
+
+
+const httpServer = http.createServer(app);
+httpServer.listen(app.get('port'), app.get('host'), () => {
+    console.log(`HTTP server running on port: ${httpServer.address().port}`);
 });
+
+try {
+    const credentials = {
+        key: fs.readFileSync('/etc/letsencrypt/live/medpoints.online/privkey.pem', 'utf8'),
+        cert: fs.readFileSync('/etc/letsencrypt/live/medpoints.online/cert.pem', 'utf8'),
+        ca: fs.readFileSync('/etc/letsencrypt/live/medpoints.online/chain.pem', 'utf8'),
+    };
+
+    const httpsServer = https.createServer(credentials, app);
+
+    httpsServer.listen(443, () => {
+        console.log('HTTPS server running on port: 443');
+    });
+
+} catch (e) {
+    console.log(`Error: Cannot start HTTPS server: ${e.message}`);
+}
+
+
+
+
+
+// const httpServer = app.listen(app.get('port'), app.get('host'), () => {
+//     console.log(`Express running → PORT ${httpServer.address().port}`);
+// });
