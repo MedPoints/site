da4166df050bf7069dc8de7f1b2293da3607722c
k0t0v <maxim.kotov@tutanota.com>
2020-03-26 17:16:29 +0300

    add cors to cabinets

--- routes/cabinets.js
+++ routes/cabinets.js
@@ -1,6 +1,13 @@
 const router = require('express').Router();
 const cabinets = require('../controllers/cabinets');
 
+router.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ["*"]);
+  res.append('Access-Control-Allow-Methods', 'GET');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+})
+
 router.get('/get-doctor', cabinets.getDoctor);
 router.get('/get-doctor/:id', cabinets.getDoctor);
 
