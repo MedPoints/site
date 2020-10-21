0ea7dcc7f9a7b9c40ad6a74fe2885e30b0a8271d
k0t0v <maxim.kotov@tutanota.com>
2020-10-21 23:58:41 +0300

    add registered users lable

--- routes/pages.js
+++ routes/pages.js
@@ -1,103 +1,110 @@
-const router = require('express').Router();
-const catchErrors = require('../handlers/errorHandlers').catchErrors;
+const router = require("express").Router();
+const catchErrors = require("../handlers/errorHandlers").catchErrors;
+const config = require("config");
+const axios = require("axios");
+const API_URL = config.get("API_URL");
 
-const auth = require('../controllers/auth');
-const explorer = require('../controllers/explorer');
-const tickets = require('../controllers/tickets');
-const rates = require('../controllers/rates');
-const favourites = require('../controllers/favourites');
-const booking = require('../controllers/booking');
+const auth = require("../controllers/auth");
+const explorer = require("../controllers/explorer");
+const tickets = require("../controllers/tickets");
+const rates = require("../controllers/rates");
+const favourites = require("../controllers/favourites");
+const booking = require("../controllers/booking");
 
-const Localization = require('../helpers/localization').Localization;
+const Localization = require("../helpers/localization").Localization;
 
-router.get('/about', (req, res) => {
-    const localization = new Localization(req.cookies.locale);
-    res.render('pages/about', {
-        PAGE_TITLE: localization.localize('titles.aboutUs'), 
-        pageName: 'about', 
-        title: localization.localize('titles.aboutUs')
-    })
+router.get("/about", (req, res) => {
+  const localization = new Localization(req.cookies.locale);
+  res.render("pages/about", {
+    PAGE_TITLE: localization.localize("titles.aboutUs"),
+    pageName: "about",
+    title: localization.localize("titles.aboutUs"),
+  });
 });
-router.get('/terms-of-use', (req, res) => { 
-    const localization = new Localization(req.cookies.locale);
-    res.render('pages/terms-of-use', {
-        title: localization.localize('titles.termsOfUse'), 
-        PAGE_TITLE: localization.localize('titles.termsOfUse'),
-        req,
-    });
+router.get("/terms-of-use", (req, res) => {
+  const localization = new Localization(req.cookies.locale);
+  res.render("pages/terms-of-use", {
+    title: localization.localize("titles.termsOfUse"),
+    PAGE_TITLE: localization.localize("titles.termsOfUse"),
+    req,
+  });
 });
-router.get('/privacy-policy', (req, res) => {
-    const localization = new Localization(req.cookies.locale);
-    res.render('pages/privacy-policy', {
-        title: localization.localize('titles.privacyPolicy'), 
-        PAGE_TITLE: localization.localize('titles.privacyPolicy'),
-        req,
-    });
+router.get("/privacy-policy", (req, res) => {
+  const localization = new Localization(req.cookies.locale);
+  res.render("pages/privacy-policy", {
+    title: localization.localize("titles.privacyPolicy"),
+    PAGE_TITLE: localization.localize("titles.privacyPolicy"),
+    req,
+  });
 });
-router.get('/privacy-terms-and-conditions', (req, res) => {
-    const localization = new Localization(req.cookies.locale);
-    res.render('pages/privacy-terms-and-conditions', {
-        title: localization.localize('titles.privacyPolicy'), 
-        PAGE_TITLE: localization.localize('titles.privacyPolicy'),
-        req,
-    });
+router.get("/privacy-terms-and-conditions", (req, res) => {
+  const localization = new Localization(req.cookies.locale);
+  res.render("pages/privacy-terms-and-conditions", {
+    title: localization.localize("titles.privacyPolicy"),
+    PAGE_TITLE: localization.localize("titles.privacyPolicy"),
+    req,
+  });
 });
-router.get('/partnership', (req, res) => {
-    const localization = new Localization(req.cookies.locale);
-    res.render('pages/partnership', {
-        title: localization.localize('titles.partnership'), 
-        PAGE_TITLE: localization.localize('titles.partnership'),
-        req,
-    });
+router.get("/partnership", (req, res) => {
+  const localization = new Localization(req.cookies.locale);
+  res.render("pages/partnership", {
+    title: localization.localize("titles.partnership"),
+    PAGE_TITLE: localization.localize("titles.partnership"),
+    req,
+  });
 });
-router.get('/explorer', catchErrors(explorer.getBlocks));
-router.get('/faq', (req, res) => {
-    const localization = new Localization(req.cookies.locale);
-    res.render('pages/faq', {
-        title: localization.localize('titles.faq'), 
-        PAGE_TITLE: localization.localize('titles.faq')
-    });
+router.get("/explorer", catchErrors(explorer.getBlocks));
+router.get("/faq", (req, res) => {
+  const localization = new Localization(req.cookies.locale);
+  res.render("pages/faq", {
+    title: localization.localize("titles.faq"),
+    PAGE_TITLE: localization.localize("titles.faq"),
+  });
 });
 
 //router.get('/sitemap', (req, res) => res.render('pages/sitemap', {PAGE_TITLE: 'Sitemap', pageName: 'sitemap', title: 'MedPoints™ Sitemap'}));
-router.get('/registration', (req, res) => {
-    const localization = new Localization(req.cookies.locale);
-    res.render('pages/registration', {
-        title: localization.localize('titles.registration'), 
-        PAGE_TITLE: localization.localize('titles.registration')
-    });
+router.get("/registration", async (req, res) => {
+  const localization = new Localization(req.cookies.locale);
+  const request = await axios.get(`${API_URL}/api/users/count`);
+  const count = +request.data.result + 2800;
+  res.render("pages/registration", {
+    count,
+    title: localization.localize("titles.registration"),
+    PAGE_TITLE: localization.localize("titles.registration"),
+  });
 });
 
-
-router.get('/search-results', (req, res) => res.render('pages/search-results', {title: 'MedPoints™ Search'}));
-router.get('/success', (req, res) => {
-    const localization = new Localization(req.cookies.locale);
-    res.render('pages/success', {
-        title: localization.localize('titles.success'), 
-        PAGE_TITLE: localization.localize('titles.success')
-    });
+router.get("/search-results", (req, res) =>
+  res.render("pages/search-results", { title: "MedPoints™ Search" })
+);
+router.get("/success", (req, res) => {
+  const localization = new Localization(req.cookies.locale);
+  res.render("pages/success", {
+    title: localization.localize("titles.success"),
+    PAGE_TITLE: localization.localize("titles.success"),
+  });
 });
-router.get('/confirm', (req, res) => {
-    const localization = new Localization(req.cookies.locale);
-    res.render('pages/confirm', {
-        title: localization.localize('titles.confirm'), 
-        PAGE_TITLE: localization.localize('titles.confirm')
-    });
+router.get("/confirm", (req, res) => {
+  const localization = new Localization(req.cookies.locale);
+  res.render("pages/confirm", {
+    title: localization.localize("titles.confirm"),
+    PAGE_TITLE: localization.localize("titles.confirm"),
+  });
 });
-router.get('/confirmation', catchErrors(auth.confirm));
-router.get('/text', (req, res) => res.render('pages/text'));
+router.get("/confirmation", catchErrors(auth.confirm));
+router.get("/text", (req, res) => res.render("pages/text"));
 
-router.get('/booking', catchErrors(booking.booking));
-router.post('/booking/details', catchErrors(booking.details));
-router.get('/rates', catchErrors(rates.getRates));
-router.post('/calculateRates', catchErrors(rates.calculateRates));
+router.get("/booking", catchErrors(booking.booking));
+router.post("/booking/details", catchErrors(booking.details));
+router.get("/rates", catchErrors(rates.getRates));
+router.post("/calculateRates", catchErrors(rates.calculateRates));
 
-router.get('/account-tickets', catchErrors(tickets.getTickets));
-router.get('/add-ticket', catchErrors(tickets.addTickets));
-router.post('/createTicket', catchErrors(tickets.createTicket));
-router.post('/askQuestion', catchErrors(tickets.sendQuestion));
+router.get("/account-tickets", catchErrors(tickets.getTickets));
+router.get("/add-ticket", catchErrors(tickets.addTickets));
+router.post("/createTicket", catchErrors(tickets.createTicket));
+router.post("/askQuestion", catchErrors(tickets.sendQuestion));
 
-router.post('/addFavourites', catchErrors(favourites.addFavourites));
-router.post('/removeFavourites', catchErrors(favourites.removeFavourites));
+router.post("/addFavourites", catchErrors(favourites.addFavourites));
+router.post("/removeFavourites", catchErrors(favourites.removeFavourites));
 
 module.exports = router;
